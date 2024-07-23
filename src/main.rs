52d43c46e365f16cef2a74cfa1eb700481258915
vedhavyas <vedhavyas.singareddi@gmail.com>
2024-07-23 15:18:47 +0530

    calculate staked slashed balances for each nominator

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod types;
 
-use crate::types::{Deposit, NominatorStorage, OperatorNominators, Withdrawal};
+use crate::types::{
+    Deposit, DomainEpoch, NominatorStorage, Operator, OperatorNominators, PendingDeposit,
+    SharePrice, Withdrawal, WithdrawalInBalance, WithdrawalInShares,
+};
 use codec::Decode;
 use futures::future::join_all;
 use sp_domains::OperatorId;
+use sp_runtime::traits::Zero;
 use std::collections::BTreeMap;
 use substrate_api_client::ac_primitives::{AssetRuntimeConfig, Config};
 use substrate_api_client::rpc::JsonrpseeClient;
@@ -11,6 +15,7 @@ use substrate_api_client::{Api as SApi, GetChainInfo, GetStorage};
 
 type Balance = <AssetRuntimeConfig as Config>::Balance;
 type Number = <AssetRuntimeConfig as Config>::BlockNumber;
+type Hash = <AssetRuntimeConfig as Config>::Hash;
 type AccountId = <AssetRuntimeConfig as Config>::AccountId;
 type Api = SApi<AssetRuntimeConfig, JsonrpseeClient>;
 
@@ -20,26 +25,49 @@ async fn main() {
         .await
         .unwrap();
     let api = SApi::<AssetRuntimeConfig, _>::new(client).await.unwrap();
-    let slashed_operators = get_slashed_operators();
+    let slashed_operators = get_slashed_operators(&api).await;
     let fut_storages: Vec<_> = slashed_operators
+        .clone()
         .into_iter()
         .map(|slashed_operator| {
             get_nominator_deposits_and_withdrawal(&api, slashed_operator.0, slashed_operator.1)
         })
         .collect();
-
     let operator_nominators = join_all(fut_storages).await;
-    for operator_nominator in operator_nominators {
-        println!(
-            "Operator[{:?}] has {:?} Nominators",
-            operator_nominator.operator_id,
-            operator_nominator.nominator_storage.len()
-        )
+
+    let operator_info_futs = slashed_operators
+        .into_iter()
+        .map(|(operator_id, block_hash)| get_operator_info(&api, operator_id, block_hash));
+    let operators_info = BTreeMap::from_iter(join_all(operator_info_futs).await);
+
+    let futs: Vec<_> = operator_nominators
+        .into_iter()
+        .map(|operator_nominator| {
+            let (operator, block_hash) = operators_info
+                .get(&operator_nominator.operator_id)
+                .cloned()
+                .unwrap();
+            calculate_nominators_slashed_amount(
+                &api,
+                operator_nominator.operator_id,
+                operator,
+                operator_nominator.nominator_storage,
+                block_hash,
+            )
+        })
+        .collect();
+
+    let nominator_slashed_balances = join_all(futs).await;
+    for (operator_id, balances) in nominator_slashed_balances {
+        println!("Operator: {:?}", operator_id);
+        for (nominator_id, balance) in balances {
+            println!("\t Nominator[{:?}]: {:?}", nominator_id, balance)
+        }
     }
 }
 
-fn get_slashed_operators() -> Vec<(OperatorId, Number)> {
-    vec![
+async fn get_slashed_operators(api: &Api) -> Vec<(OperatorId, Hash)> {
+    let slashed_operators = vec![
         (65, 2364057),
         (41, 2364307),
         (64, 2364389),
@@ -52,25 +80,38 @@ fn get_slashed_operators() -> Vec<(OperatorId, Number)> {
         (37, 2368542),
         (77, 2368906),
         (40, 2369910),
-    ]
+    ];
+
+    let futs: Vec<_> = slashed_operators
+        .into_iter()
+        .map(|(operator_id, number)| async move {
+            (
+                operator_id,
+                api.get_block_hash(Some(number - 1))
+                    .await
+                    .ok()
+                    .flatten()
+                    .unwrap(),
+            )
+        })
+        .collect();
+    join_all(futs).await
 }
 
 async fn get_nominator_deposits_and_withdrawal(
     api: &Api,
     operator_id: OperatorId,
-    slashed_block_number: Number,
+    block_hash: Hash,
 ) -> OperatorNominators {
-    let deposits =
-        get_nominator_storage::<Deposit>(api, operator_id, slashed_block_number, "Deposits").await;
+    let deposits = get_nominator_storage::<Deposit>(api, operator_id, block_hash, "Deposits").await;
     let withdrawals =
-        get_nominator_storage::<Withdrawal>(api, operator_id, slashed_block_number, "Withdrawals")
-            .await;
+        get_nominator_storage::<Withdrawal>(api, operator_id, block_hash, "Withdrawals").await;
     let mut storage = BTreeMap::new();
     deposits.into_iter().for_each(|(nominator_id, deposit)| {
         storage.insert(
             nominator_id,
             NominatorStorage {
-                deposit: Some(deposit),
+                deposit,
                 withdrawal: None,
             },
         );
@@ -78,19 +119,19 @@ async fn get_nominator_deposits_and_withdrawal(
     withdrawals
         .into_iter()
         .for_each(|(nominator_id, withdrawal)| {
-            let nominator_storage = match storage.get(&nominator_id) {
-                None => NominatorStorage {
-                    deposit: None,
-                    withdrawal: Some(withdrawal),
-                },
-                Some(nominator_storage) => NominatorStorage {
-                    deposit: nominator_storage.deposit.clone(),
-                    withdrawal: None,
-                },
+            match storage.get(&nominator_id) {
+                // there will always be a deposit for this nominator even with zero shares
+                None => return,
+                Some(nominator_storage) => storage.insert(
+                    nominator_id,
+                    NominatorStorage {
+                        deposit: nominator_storage.deposit.clone(),
+                        withdrawal: Some(withdrawal),
+                    },
+                ),
             };
-            storage.insert(nominator_id, nominator_storage);
         });
-    let nominator_count = get_nominator_count(api, operator_id, slashed_block_number).await;
+    let nominator_count = get_nominator_count(api, operator_id, block_hash).await;
     assert_eq!(storage.len() as u32, nominator_count);
     OperatorNominators {
         operator_id,
@@ -101,15 +142,9 @@ async fn get_nominator_deposits_and_withdrawal(
 async fn get_nominator_storage<V: Decode>(
     api: &Api,
     operator_id: OperatorId,
-    slashed_block_number: Number,
+    block_hash: Hash,
     storage: &'static str,
 ) -> Vec<(AccountId, V)> {
-    let block_hash = api
-        .get_block_hash(Some(slashed_block_number - 1))
-        .await
-        .ok()
-        .flatten()
-        .unwrap();
     let storage_prefix = api
         .get_storage_double_map_key_prefix("Domains", storage, operator_id)
         .await
@@ -147,17 +182,7 @@ async fn get_nominator_storage<V: Decode>(
     join_all(storage_futures).await
 }
 
-async fn get_nominator_count(
-    api: &Api,
-    operator_id: OperatorId,
-    slashed_block_number: Number,
-) -> u32 {
-    let block_hash = api
-        .get_block_hash(Some(slashed_block_number - 1))
-        .await
-        .ok()
-        .flatten()
-        .unwrap();
+async fn get_nominator_count(api: &Api, operator_id: OperatorId, block_hash: Hash) -> u32 {
     let count = api
         .get_storage_map::<_, u32>("Domains", "NominatorCount", operator_id, Some(block_hash))
         .await
@@ -167,4 +192,178 @@ async fn get_nominator_count(
 
     // + 1 since operator's nominator is not counted
     count + 1
+}
+
+async fn get_operator_info(
+    api: &Api,
+    operator_id: OperatorId,
+    block_hash: Hash,
+) -> (OperatorId, (Operator, Hash)) {
+    (
+        operator_id,
+        (
+            api.get_storage_map::<_, Operator>(
+                "Domains",
+                "Operators",
+                operator_id,
+                Some(block_hash),
+            )
+            .await
+            .ok()
+            .flatten()
+            .unwrap(),
+            block_hash,
+        ),
+    )
+}
+
+async fn calculate_nominators_slashed_amount(
+    api: &Api,
+    operator_id: OperatorId,
+    mut operator: Operator,
+    operator_nominators: BTreeMap<AccountId, NominatorStorage>,
+    block_hash: Hash,
+) -> (OperatorId, Vec<(AccountId, Balance)>) {
+    let mut total_stake = operator
+        .current_total_stake
+        .checked_add(operator.current_epoch_rewards)
+        .unwrap();
+
+    operator.current_epoch_rewards = Zero::zero();
+    let mut total_shares = operator.current_total_shares;
+    let share_price = SharePrice::new(total_shares, total_stake);
+
+    let mut total_storage_fee_deposit = operator.total_storage_fee_deposit;
+
+    let mut nominators_slashed_balances = vec![];
+    for (nominator_id, mut nominator_storage) in operator_nominators {
+        do_convert_previous_epoch_deposits(
+            api,
+            operator_id,
+            &mut nominator_storage.deposit,
+            block_hash,
+        )
+        .await;
+
+        let (amount_ready_to_withdraw, shares_withdrew_in_current_epoch) = match nominator_storage
+            .withdrawal
+        {
+            None => (Zero::zero(), Zero::zero()),
+            Some(mut withdrawal) => {
+                do_convert_previous_epoch_withdrawal(api, operator_id, &mut withdrawal, block_hash)
+                    .await;
+                (
+                    withdrawal.total_withdrawal_amount,
+                    withdrawal
+                        .withdrawal_in_shares
+                        .map(|WithdrawalInShares { shares, .. }| shares)
+                        .unwrap_or_default(),
+                )
+            }
+        };
+
+        let nominator_shares = nominator_storage
+            .deposit
+            .known
+            .shares
+            .checked_add(shares_withdrew_in_current_epoch)
+            .unwrap();
+
+        let nominator_staked_amount = share_price.shares_to_stake(nominator_shares);
+        total_stake = total_stake.saturating_sub(nominator_staked_amount);
+        total_shares = total_shares.saturating_sub(nominator_shares);
+
+        nominators_slashed_balances.push((nominator_id, nominator_staked_amount))
+    }
+
+    (operator_id, nominators_slashed_balances)
+}
+
+async fn do_convert_previous_epoch_deposits(
+    api: &Api,
+    operator_id: OperatorId,
+    deposit: &mut Deposit,
+    block_hash: Hash,
+) {
+    // if it is one of the previous domain epoch, then calculate shares for the epoch and update known deposit
+    let pending_deposit = match deposit.pending.take() {
+        None => return,
+        Some(pd) => pd,
+    };
+
+    let PendingDeposit {
+        effective_domain_epoch,
+        amount,
+        storage_fee_deposit,
+    } = pending_deposit;
+
+    if let Some(epoch_share_price) =
+        get_operator_epoch_share_price(api, operator_id, effective_domain_epoch, block_hash).await
+    {
+        let new_shares = epoch_share_price.stake_to_shares(amount);
+        deposit.known.shares = deposit.known.shares.checked_add(new_shares).unwrap();
+        deposit.known.storage_fee_deposit = deposit
+            .known
+            .storage_fee_deposit
+            .checked_add(storage_fee_deposit)
+            .unwrap();
+    }
+}
+
+async fn do_convert_previous_epoch_withdrawal(
+    api: &Api,
+    operator_id: OperatorId,
+    withdrawal: &mut Withdrawal,
+    block_hash: Hash,
+) {
+    let pending_withdrawal = match withdrawal.withdrawal_in_shares.take() {
+        None => return,
+        Some(pw) => pw,
+    };
+
+    let WithdrawalInShares {
+        domain_epoch,
+        unlock_at_confirmed_domain_block_number,
+        shares,
+        storage_fee_refund,
+    } = pending_withdrawal;
+
+    if let Some(epoch_share_price) =
+        get_operator_epoch_share_price(api, operator_id, domain_epoch, block_hash).await
+    {
+        let withdrawal_amount = epoch_share_price.shares_to_stake(shares);
+
+        withdrawal.total_withdrawal_amount = withdrawal
+            .total_withdrawal_amount
+            .checked_add(withdrawal_amount)
+            .unwrap();
+
+        let (domain_id, _) = domain_epoch.deconstruct();
+
+        let withdraw_in_balance = WithdrawalInBalance {
+            domain_id,
+            unlock_at_confirmed_domain_block_number,
+            amount_to_unlock: withdrawal_amount,
+            storage_fee_refund,
+        };
+        withdrawal.withdrawals.push_back(withdraw_in_balance);
+    }
+}
+
+async fn get_operator_epoch_share_price(
+    api: &Api,
+    operator_id: OperatorId,
+    domain_epoch: DomainEpoch,
+    block_hash: Hash,
+) -> Option<SharePrice> {
+    api.get_storage_double_map::<_, _, SharePrice>(
+        "Domains",
+        "OperatorEpochSharePrice",
+        operator_id,
+        domain_epoch,
+        Some(block_hash),
+    )
+    .await
+    .ok()
+    .flatten()
 }
--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,9 @@
 use crate::{AccountId, Balance, Number};
 use codec::{Decode, Encode};
 use scale_info::TypeInfo;
-use sp_domains::{DomainId, EpochIndex, OperatorId};
+use sp_domains::{DomainId, EpochIndex, OperatorId, OperatorPublicKey};
+use sp_runtime::traits::Zero;
+use sp_runtime::{Perbill, Percent};
 use std::collections::{BTreeMap, VecDeque};
 
 #[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq, Default)]
@@ -25,6 +27,12 @@ pub(crate) struct PendingDeposit {
 
 #[derive(TypeInfo, Debug, Encode, Decode, Copy, Clone, PartialEq, Eq)]
 pub struct DomainEpoch(DomainId, EpochIndex);
+
+impl DomainEpoch {
+    pub(crate) fn deconstruct(&self) -> (DomainId, EpochIndex) {
+        (self.0, self.1)
+    }
+}
 
 #[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq, Default)]
 pub(crate) struct Withdrawal {
@@ -55,11 +63,84 @@ pub(crate) struct WithdrawalInShares {
 }
 
 pub(crate) struct NominatorStorage {
-    pub(crate) deposit: Option<Deposit>,
+    pub(crate) deposit: Deposit,
     pub(crate) withdrawal: Option<Withdrawal>,
 }
 
 pub(crate) struct OperatorNominators {
     pub(crate) operator_id: OperatorId,
     pub(crate) nominator_storage: BTreeMap<AccountId, NominatorStorage>,
+}
+
+#[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq)]
+pub struct OperatorDeregisteredInfo {
+    pub domain_epoch: DomainEpoch,
+    pub unlock_at_confirmed_domain_block_number: Number,
+}
+
+#[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq)]
+pub enum OperatorStatus {
+    Registered,
+    /// De-registered at given domain epoch.
+    Deregistered(OperatorDeregisteredInfo),
+    Slashed,
+    PendingSlash,
+}
+
+#[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq)]
+pub struct Operator {
+    pub signing_key: OperatorPublicKey,
+    pub current_domain_id: DomainId,
+    pub next_domain_id: DomainId,
+    pub minimum_nominator_stake: Balance,
+    pub nomination_tax: Percent,
+    /// Total active stake of combined nominators under this operator.
+    pub current_total_stake: Balance,
+    /// Total rewards this operator received this current epoch.
+    pub current_epoch_rewards: Balance,
+    /// Total shares of all the nominators under this operator.
+    pub current_total_shares: Balance,
+    /// The status of the operator, it may be stale due to the `OperatorStatus::PendingSlash` is
+    /// not assigned to this field directly, thus MUST use the `status()` method to query the status
+    /// instead.
+    /// TODO: update the filed to `_status` to avoid accidental access in next network reset
+    status: OperatorStatus,
+    /// Total deposits during the previous epoch
+    pub deposits_in_epoch: Balance,
+    /// Total withdrew shares during the previous epoch
+    pub withdrawals_in_epoch: Balance,
+    /// Total balance deposited to the bundle storage fund
+    pub total_storage_fee_deposit: Balance,
+}
+
+#[derive(TypeInfo, Debug, Encode, Decode, Clone, PartialEq, Eq, Default)]
+pub struct SharePrice(Perbill);
+
+impl SharePrice {
+    /// Creates a new instance of share price from shares and stake.
+    pub(crate) fn new(shares: Balance, stake: Balance) -> Self {
+        SharePrice(if shares.is_zero() || stake.is_zero() {
+            Perbill::one()
+        } else {
+            Perbill::from_rational(shares, stake.into())
+        })
+    }
+
+    /// Converts stake to shares based on the share price
+    pub(crate) fn stake_to_shares(&self, stake: Balance) -> Balance {
+        if self.0.is_one() {
+            stake.into()
+        } else {
+            self.0.mul_floor(stake).into()
+        }
+    }
+
+    /// Converts shares to stake based on the share price
+    pub(crate) fn shares_to_stake(&self, shares: Balance) -> Balance {
+        if self.0.is_one() {
+            shares.into()
+        } else {
+            self.0.saturating_reciprocal_mul_floor(shares.into())
+        }
+    }
 }
